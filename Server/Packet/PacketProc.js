6839410c4f1bbd3373b272d73676252c0623f744
성원 정 <tyhg56@naver.com>
2024-10-04 17:05:26 +0900

    Fix : " PacketProc 수정 "
    PacketProc
     - 디버깅 위해서 임시로 만든 console.log 제거
     - ScoreUpdate 추가 ( 점수 업데이트 )

--- Server/Packet/PacketProc.js
+++ Server/Packet/PacketProc.js
@@ -1,13 +1,12 @@
 import { getGameAssets } from "../Contents/assets.js"
 
 // 서버에서 게임시작
-export const GameStart = (uuid, payload, Stage) => {    
+export const GameStart = (uuid, payload, Stage) => {
     const { stages } = getGameAssets();
 
     Stage.ClearStage(uuid);
 
     Stage.SetStage(uuid, stages.data[0].id, payload.timestamp);
-    console.log("스테이지: ", Stage.GetStage(uuid));
 
     return { status: "게임시작" };
 }
@@ -18,33 +17,32 @@ export const GameEnd = (uuid, payload, Stage) => {
 }
 
 // 스테이지 옮기기
-export const MoveStage = (uuid, payload, Stage) => {        
-    let currentStages = Stage.GetStage(uuid);    
-    if(!currentStages.length)
-    {
-        return { status: "실패", message:"[MoveStage] 스테이지를 찾을 수 없습니다."};
+export const MoveStage = (uuid, payload, Stage) => {
+    let currentStages = Stage.GetStage(uuid);
+    if (!currentStages.length) {
+        return { status: "실패", message: "[MoveStage] 스테이지를 찾을 수 없습니다." };
     }
 
-    currentStages.sort((a,b) => a.id - b.id);
+    currentStages.sort((a, b) => a.id - b.id);
     const currentStage = currentStages[currentStages.length - 1];
-
-    console.log(currentStage);
 
     if (currentStage.id !== payload.currentStage) {
         return { status: "실패", message: "현재 스테이지가 맞지 않습니다." };
     }
-    
+
     const serverTime = Date.now(); // 현재 서버 시간
     const elapsedTime = (serverTime - currentStage.timestamp) / 1000;
 
-    const { stages} = getGameAssets();
+    const { stages } = getGameAssets();
     if (!stages.data.some((stage) => stage.id === payload.targetStage)) {
         return { status: "실패", message: "목표 스테이지를 찾을 수 없습니다." }
     }
 
     Stage.SetStage(uuid, payload.targetStage, serverTime);
 
-    console.log(currentStages);
-
     return { status: "스테이지 옮기기 성공" }
+}
+
+export const ScoreUpdate = (uuid, payload, Stage) => {
+
 }
