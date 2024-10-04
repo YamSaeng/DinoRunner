4f9c796f62a15f395f1970bf48717aca8ed7afac
성원 정 <tyhg56@naver.com>
2024-10-04 21:58:43 +0900

    Fix : " Score 수정 "
    Client
     - Score
      - start 변수 추가 ( 서버에서 데이터를 받았는지 여부 )
      - update() 수정
       - start 가 활성화 된 경우에만 점수를 계산하도록 수정
       - 점수가 goalScore가 될 경우 스테이지 이동 패킷 서버로 전송
       - 일정 초마다 점수를 서버와 동기화

--- Client/Score.js
+++ Client/Score.js
@@ -1,15 +1,45 @@
+import { C2S_PACKET_TYPE_MOVE_STAGE, C2S_PACKET_TYPE_SCORE_UPDATE, C2S_SCORE_SEND_TIME } from "./Constant.js";
+import Session from "./Network/Session.js";
+
 class Score {
   score = 0;
   HIGH_SCORE_KEY = 'highScore';
+  stageChange = true;
 
   constructor(ctx, scaleRatio) {
     this.ctx = ctx;
     this.canvas = ctx.canvas;
     this.scaleRatio = scaleRatio;
+    this.serverScoreUpdateTime = C2S_SCORE_SEND_TIME;
+    this.scoreMultiple = 0;
+    this.goalScore = 0;
+    this.start = false;
   }
 
-  update(deltaTime) {
-    this.score += deltaTime * 0.001;
+  update(deltaTime) {        
+    if (this.start == true) {
+      this.score += deltaTime * 0.001 * this.scoreMultiple;
+
+      if (Math.floor(this.score) === this.goalScore && this.stageChange) {        
+        this.stageChange = false;
+        Session.GetInstance().SendEvent(C2S_PACKET_TYPE_MOVE_STAGE, { currentStage: 1000, targetStage: 1001 });
+      }
+
+      if (this.serverScoreUpdateTime < 0) {
+        this.serverScoreUpdateTime = C2S_SCORE_SEND_TIME;
+
+        Session.GetInstance().SendEvent(C2S_PACKET_TYPE_SCORE_UPDATE, { score: Math.floor(this.score) });
+      }
+
+      this.serverScoreUpdateTime -= deltaTime;
+    }    
+  }
+
+  SetScoreInfo(goalScore, scoreMultiple) {
+    this.goalScore = goalScore;
+    this.scoreMultiple = scoreMultiple;
+
+    this.start = true;
   }
 
   getItem(itemId) {
@@ -31,6 +61,15 @@ class Score {
     return this.score;
   }
 
+  SetScoreMinus(score)
+  {
+    this.score -= score;
+    if(this.score < 0)
+    {
+      this.score = 0;
+    }
+  }
+
   draw() {
     const highScore = Number(localStorage.getItem(this.HIGH_SCORE_KEY));
     const y = 25 * this.scaleRatio;
@@ -40,11 +79,11 @@ class Score {
     this.ctx.fillStyle = '#525250';
 
     const scoreX = this.canvas.width - 75 * this.scaleRatio;
-    const highScoreX = scoreX - 125 * this.scaleRatio;            
+    const highScoreX = scoreX - 125 * this.scaleRatio;
 
     const scorePadded = Math.floor(this.score).toString().padStart(6, 0);
     const highScorePadded = highScore.toString().padStart(6, 0);
-    
+
     this.ctx.fillText(`HI ${highScorePadded}`, highScoreX, y);
     this.ctx.fillText(scorePadded, scoreX, y);
   }
