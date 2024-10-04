21f2a38b17de655ccbd3a72635bbd7db1ba0eb09
성원 정 <tyhg56@naver.com>
2024-10-04 21:53:42 +0900

    docs : " Constant 수정 "
    Client
     - Constant
      - MAIN_GAME_CANVAS_WIDTH 추가 ( 메인 게임 캔버스 너비 )
      - MAIN_GAME_CANVAS_HEIGHT 추가 ( 메인 게임 캔버스 높이 )
      - RANK_SCORE_CANVAS_WIDTH 추가 ( 랭킹 캔버스 너비 )
      - RANK_SCORE_CANVAS_HEIGHT 추가 ( 랭킹 캔버스 높이 )
    
      - C2S_SCORE_SEND_TIME 추가 ( 점수 업데이트 시간 )
      - C2S_PACKET_TYPE_GAME_INIT 추가 ( 게임 초기화 )
      - S2C_PACKET_TYPE_GAME_INIT 추가 ( 게임 초기화 )
      - S2C_PACKET_TYPE_RANK_SCORE_UPDATE 추가 ( 랭킹 점수 업데이트 )
    
    Server
     - Constant
      - S2C_PACKET_TYPE_GAME_INIT ( 게임 초기화 )
      - S2C_PACKET_TYPE_GAME_START ( 게임 시작 )
      - S2C_PACKET_TYPE_RANK_SCORE_UPDATE ( 랭킹 점수 )

--- Client/Constant.js
+++ Client/Constant.js
@@ -12,14 +12,17 @@ export const GAME_SPEED_START = 1;
 export const GAME_SPEED_INCREMENT = 0.00001;
 
 // 게임 크기
-export const GAME_WIDTH = 900;
-export const GAME_HEIGHT = 200;
+export const MAIN_GAME_CANVAS_WIDTH = 900;
+export const MAIN_GAME_CANVAS_HEIGHT = 200;
+
+export const RANK_SCORE_CANVAS_WIDTH = 300;
+export const RANK_SCORE_CANVAS_HEIGHT = 300;
 
 // 플레이어
 // 800 * 200 사이즈의 캔버스에서는 이미지의 기본크기가 크기때문에 1.5로 나눈 값을 사용. (비율 유지)
 export const PLAYER_WIDTH = 88 / 1.5;
 export const PLAYER_HEIGHT = 94 / 1.5;
-export const MAX_JUMP_HEIGHT = GAME_HEIGHT;
+export const MAX_JUMP_HEIGHT = MAIN_GAME_CANVAS_HEIGHT;
 export const MIN_JUMP_HEIGHT = 150;
 
 // 땅
@@ -41,8 +44,15 @@ export const OBJECT_TYPE_CACTUS = 104;
 
 export const JOB_TYPE_CREATE_OBJECT_FIRE = 1001;
 
+export const C2S_SCORE_SEND_TIME = 1000;
+
+export const C2S_PACKET_TYPE_GAME_INIT = 10;
 export const C2S_PACKET_TYPE_GAME_START = 11;
 export const C2S_PACKET_TYPE_GAME_END = 12;
 
 export const C2S_PACKET_TYPE_MOVE_STAGE = 101;
+
 export const C2S_PACKET_TYPE_SCORE_UPDATE = 102;
+export const S2C_PACKET_TYPE_GAME_INIT = 103;
+export const S2C_PACKET_TYPE_GAME_START = 104;
+export const S2C_PACKET_TYPE_RANK_SCORE_UPDATE = 105;
