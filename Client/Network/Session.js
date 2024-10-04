3bca844a9f18229ef8225093827ccd8500158442
성원 정 <tyhg56@naver.com>
2024-10-04 14:26:49 +0900

    Feat : " Session 추가 "
    Session 추가
     - Server와 통신
     - Connect()
      - 서버와 연결
     - SendEvent()
      - 서버에게 패킷 전송

---
+++ Client/Network/Session.js
@@ -0,0 +1,40 @@
+import { CLIENT_VERSION, PORT } from "../Constant.js";
+
+class Session {
+    constructor() {
+        this.userId = null;
+        this.socket = null;
+        this.gInstance = null;
+    }
+
+    static GetInstance() {
+        if (!this.gInstance) {
+            this.gInstance = new Session();
+        }
+
+        return this.gInstance;
+    }
+
+    Connect() {
+        this.socket = io(`http://localhost:${PORT}`, {
+            query: {
+                clientVersion: CLIENT_VERSION
+            }
+        });
+
+        this.socket.on("connection", (data) => {
+            this.userId = data.useruuid;
+        });
+    }
+
+    SendEvent(packetType, payload) {
+        this.socket.emit("event", {
+            userId: this.userId,
+            clientVersion: CLIENT_VERSION,
+            packetType,
+            payload,
+        });
+    }
+}
+
+export default Session;
