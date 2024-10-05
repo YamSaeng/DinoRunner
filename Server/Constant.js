2f8c224aeeaa5a7b301b472ca36ea4a895fb9ba6
성원 정 <tyhg56@naver.com>
2024-10-05 12:13:59 +0900

    docs : " Constant 수정 "
    Constant
     - S2C_PACKET_TYPE_MOVE_STAGE 추가 ( 스테이지 이동 요청 결과 받기 패킷 )

--- Server/Constant.js
+++ Server/Constant.js
@@ -5,4 +5,5 @@ export const CLIENT_VERSION = ["1.0.0", "1.0.1", "1.1.0"];
 export const S2C_PACKET_TYPE_GAME_INIT = 103;
 export const S2C_PACKET_TYPE_GAME_START = 104;
 export const S2C_PACKET_TYPE_RANK_SCORE_UPDATE = 105;
+export const S2C_PACKET_TYPE_MOVE_STAGE = 106;
 export const S2C_PACKET_TYPE_USER_DISCONNECT = 1001;
