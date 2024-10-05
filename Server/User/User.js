486ae6d4871197ae48acdc3f0f306bbcac04caef
성원 정 <tyhg56@naver.com>
2024-10-05 23:30:45 +0900

    Fix : " User 수정 "
    User
     - verifyScore 멤버 변수 추가 ( 점수 검증용 )
     - update() 추가
      - score 계산

--- Server/User/User.js
+++ Server/User/User.js
@@ -3,7 +3,15 @@ class User {
         this.userUUID = userUUID;
         this.socket = socket;
         this.score = 0;
+        this.verifyScore = 0;
         this.currentStage = 1;
+        this.scoreMultiple = 1;
+    }
+
+    update() {
+        this.score += this.scoreMultiple;
+
+        console.log(`UserID : ${this.userUUID} Score : ${this.score}`);
     }
 }
 
