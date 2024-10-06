d44df93e17cd542c3fe2b9b6a47a66a2131a6e2d
성원 정 <tyhg56@naver.com>
2024-10-07 07:33:18 +0900

    Fix : " PacketType 수정 "
    PacketType
     - GetItem, CollideCactus 이벤트 할당

--- Server/PacketType.js
+++ Server/PacketType.js
@@ -1,11 +1,12 @@
-import { GameInit, GameStart, GameEnd, MoveStage, ScoreUpdate } from "./Packet/PacketProc.js";
+import { GameInit, GameStart, GameEnd, MoveStage, GetItem, CollideCactus } from "./Packet/PacketProc.js";
 
 const packetTypeMappings = {
     10: GameInit,
     11: GameStart,
     12: GameEnd,
-    101: MoveStage,
-    102: ScoreUpdate
+    101: MoveStage,    
+    107: GetItem,
+    109: CollideCactus
 };
 
 export default packetTypeMappings;
