6cbc0556b293ebc831d130bb379e957de0803b60
성원 정 <tyhg56@naver.com>
2024-10-07 09:32:59 +0900

    docs : " Constant 수정 "
    Constant
     - RANK_SCORE_UPDATE_TIME 추가 ( 랭킹 업데이트 시간 )

--- Server/Constant.js
+++ Server/Constant.js
@@ -3,6 +3,7 @@ export const PORT = 3020;
 export const CLIENT_VERSION = ["1.0.0", "1.0.1", "1.1.0"];
 
 export const USER_SCORE_UPDATE_TIME = 1000;
+export const RANK_SCORE_UPDATE_TIME = 500;
 
 export const S2C_PACKET_TYPE_GAME_INIT = 103;
 export const S2C_PACKET_TYPE_GAME_START = 104;
