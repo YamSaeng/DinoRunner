8daf9c6b35a4b4d968d332a60aa8df1975354ce9
성원 정 <tyhg56@naver.com>
2024-10-07 12:53:31 +0900

    Comment : " PacketType 주석 "
    PacketType 주석 달기

--- Server/PacketType.js
+++ Server/PacketType.js
@@ -1,12 +1,12 @@
 import { GameInit, GameStart, GameEnd, MoveStage, GetItem, CollideCactus } from "./Packet/PacketProc.js";
 
 const packetTypeMappings = {
-    10: GameInit,
-    11: GameStart,
-    12: GameEnd,
-    101: MoveStage,    
-    107: GetItem,
-    109: CollideCactus
+    10: GameInit, // 게임 초기화
+    11: GameStart, // 게임 시작
+    12: GameEnd, // 게임 끝
+    101: MoveStage, // 스테이지 이동
+    107: GetItem, // 아이템 습득
+    109: CollideCactus // 선인장 충돌
 };
 
 export default packetTypeMappings;
