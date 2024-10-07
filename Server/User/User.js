f7cf142d214c9f597f1098986d51ca063dd670df
성원 정 <tyhg56@naver.com>
2024-10-07 09:32:12 +0900

    style : " User 수정 "
    User
     - verifyScore 멤버 변수 삭제 ( 더이상 사용하지 않음 )

--- Server/User/User.js
+++ Server/User/User.js
@@ -2,18 +2,17 @@ class User {
     constructor(userUUID, socket) {
         this.userUUID = userUUID;
         this.socket = socket;
-        this.score = 0;
-        this.verifyScore = 0;
+        this.score = 0;        
         this.currentStage = 1;
         this.scoreMultiple = 1;
     }
 
+    // 점수 업데이트
     Update() {
-        this.score += this.scoreMultiple;
-
-        console.log(`UserID : ${this.userUUID} Score : ${this.score}`);
+        this.score += this.scoreMultiple;        
     }
 
+    // 점수 설정
     SetScore(score)
     {
         this.score += score;
